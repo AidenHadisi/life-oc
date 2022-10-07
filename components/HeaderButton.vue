b3548b30af7d814f24659ad346f1d709b4e0eadb
Aiden Hadisi <so.hadisi@gmail.com>
2022-10-06 22:04:38 -0700

    main header changes

---
+++ components/HeaderButton.vue
@@ -0,0 +1,9 @@
+<template>
+  <a
+    class="p-4 uppercase opacity-100 rounded-md border text-white hover:bg-rose-800 transition-all duration-700 cursor-pointer"
+    href="https://www.facebook.com/lifechurchorangeca"
+    target="_blank"
+  >
+    <slot />
+  </a>
+</template>
--- components/TheHeader.vue
+++ components/TheHeader.vue
@@ -8,19 +8,21 @@
       autoplay
       muted
       loop
-      class="object-cover w-full h-full absolute top-0 bottom-0 opacity-60"
+      class="object-cover w-full h-full absolute top-0 bottom-0 opacity-50"
     >
       <source src="/img/video.mp4" type="video/mp4" />
     </video>
 
     <div class="relative">
       <div
-        class="text-8xl text-white font-display font-extrabold drop-shadow-lg opacity-70"
+        class="text-8xl text-white font-display font-extrabold drop-shadow-lg opacity-70 animate__animated animate__slow animate__fadeInUp"
       >
         Life Church Of Orange
       </div>
 
-      <AButton class="" />
+      <div class="mt-8 animate__animated animate__zoomIn animate__slow animate__delay-2s">
+        <HeaderButton class="text-2xl">Watch Live Stream</HeaderButton>
+      </div>
     </div>
   </div>
 </template>
