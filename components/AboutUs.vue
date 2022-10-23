9afa3802ffd2c168d9cd4344848afc966d046db8
Aiden Hadisi <so.hadisi@gmail.com>
2022-10-22 17:03:17 -0700

    faster images

--- components/AboutUs.vue
+++ components/AboutUs.vue
@@ -2,12 +2,7 @@
   <section
     class="container mx-auto p-8 flex flex-wrap lg:flex-nowrap gap-3 justify-between"
   >
-    <div
-      class="p-5"
-      data-aos="fade-right"
-      data-aos-duration="3000"
-      data-aos-offset="200"
-    >
+    <div data-aos="fade-right" data-aos-duration="3000" data-aos-offset="200">
       <h2
         class="text-3xl inline-block border-b-4 border-brown pr-8 pb-2 mb-3 text-left font-bold"
       >
@@ -22,10 +17,12 @@
       </p>
     </div>
     <nuxt-img
-      class="min-w-0 max-w-lg rounded-xl shadow-lg shadow-gray-600 ring-4 ring-brown min-h-[200px]"
+      class="rounded-xl shadow-lg shadow-gray-500 h-full"
       src="/img/img1.jpg"
       alt="exterior image"
-      quality="80"
+      width="500"
+      height="300"
+      format="webp"
       data-aos="zoom-in-left"
       data-aos-duration="3000"
       data-aos-offset="200"
